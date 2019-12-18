34f84db6f6d45ddd6363f7617be4fd5ff77da76a
raghuk9529 <william@oasys.co>
2019-12-18 16:38:35 +0530

    Fiirst commit

---
+++ Sploot_file/app/src/main/java/com/work/sploot/activities/Medicine.kt
@@ -0,0 +1,150 @@
+package com.work.sploot.activities
+
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Button
+import android.widget.Spinner
+import androidx.fragment.app.Fragment
+import com.work.sploot.R
+import com.work.sploot.SplootAppDB
+import kotlinx.android.synthetic.main.medicine.view.*
+
+class Medicine : Fragment() {
+    private var splootDB: SplootAppDB? = null
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val views = inflater.inflate(R.layout.medicine, container, false)
+
+        var vaccinationbtn=views.findViewById<Button>(R.id.vaccinationadd)
+        var dewormingbtn=views.findViewById<Button>(R.id.dewormingadd)
+        var othersbtn=views.findViewById<Button>(R.id.othersadd)
+        var notebtn=views.findViewById<Button>(R.id.noteadd)
+
+    /*    val mContext = activity
+        val manager = mContext?.supportFragmentManager
+        val transaction = manager?.beginTransaction()
+*/
+
+
+        vaccinationbtn.setOnClickListener {
+
+
+/*
+            transaction?.replace(R.id.medicfragement, vaccination_update_fragment.newInstance(1))
+
+            transaction?.addToBackStack("")
+
+            transaction?.commit()
+            views.viewformedic.visibility=View.VISIBLE
+            views.mediclist.visibility=View.GONE
+
+            */
+
+
+            val mContext = activity
+
+            val fm = mContext?.supportFragmentManager
+
+            val transaction = fm?.beginTransaction()
+
+            transaction?.replace(R.id.medicine_frag,vaccination_update_fragment.newInstance(1))
+
+            transaction?.addToBackStack("null")
+
+            transaction?.commit()
+
+        }
+        dewormingbtn.setOnClickListener {
+
+
+            val mContext = activity
+
+            val fm = mContext?.supportFragmentManager
+
+            val transaction = fm?.beginTransaction()
+
+            transaction?.replace(R.id.medicine_frag, deworming_fragment())
+
+            transaction?.addToBackStack("null")
+
+            transaction?.commit()
+/*
+            transaction?.replace(R.id.medicfragement, deworming_fragment())
+
+            transaction?.addToBackStack("")
+
+            transaction?.commit()
+
+
+            views.viewformedic.visibility=View.VISIBLE
+
+            views.mediclist.visibility=View.GONE
+
+
+            */
+
+        }
+        othersbtn.setOnClickListener {
+
+
+            val mContext = activity
+
+            val fm = mContext?.supportFragmentManager
+
+            val transaction = fm?.beginTransaction()
+
+            transaction?.replace(R.id.medicine_frag,vaccination_update_fragment.newInstance(2))
+
+            transaction?.addToBackStack("null")
+
+            transaction?.commit()
+   /*         transaction?.replace(R.id.medicfragement, vaccination_update_fragment.newInstance(2))
+
+           // transaction?.replace(R.id.medicfragement, vaccinationfrag.newInstance(3))
+
+            transaction?.addToBackStack("")
+            transaction?.commit()
+
+            views.viewformedic.visibility=View.VISIBLE
+            views.mediclist.visibility=View.GONE
+*/
+        }
+
+        notebtn.setOnClickListener {
+
+            val mContext = activity
+
+            val fm = mContext?.supportFragmentManager
+
+            val transaction = fm?.beginTransaction()
+
+            transaction?.replace(R.id.medicine_frag, othersfragment.newInstance())
+
+            transaction?.addToBackStack("null")
+
+            transaction?.commit()
+
+        /*    transaction?.replace(R.id.medicfragement, othersfragment.newInstance())
+            transaction?.addToBackStack("")
+            transaction?.commit()
+            views.viewformedic.visibility=View.VISIBLE
+            views.mediclist.visibility=View.GONE*/
+
+        }
+
+        //dewormingadd othersadd noteadd
+        return views
+    }
+
+
+
+
+}
